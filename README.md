ada166a689a4372d92ecec4170cac319b1752e67
Moe Jangda <m.jangda88@gmail.com>
2018-05-21 19:00:12 -0500

    finish initial implementation

---
+++ README.md
@@ -0,0 +1,54 @@
+# Logger
+
+A Simple extensible logging lib for nodejs. Enables users with as much functionality as they want (i.e. sending certain log messages to slack) in the form of plugins.
+
+## Usage:
+```js
+const Logger = require('logger-nodejs');
+const log = new Logger();
+
+
+log.info('info');
+log.warn('warn');
+log.error('error');
+log.fatal('fatal');
+```
+
+```
+# output:
+[INFO] <2018-5-21 18:48:21> (74539) -> info
+[WARN] <2018-5-21 18:48:21> (74539) -> warn
+[ERROR] <2018-5-21 18:48:21> (74539) -> error
+[FATAL] <2018-5-21 18:48:21> (74539) -> fatal
+```
+
+- Provide your desired log level when starting your application like so `LOG_LEVEL=error` 
+- The selected log level acts as a filter with regards to how much you want printed out. If you choose 'info' as your log level then everything from info - fatal will be printed out. On the other hand if you want to only print out the most severe errors you could select 'error' as the log level which would cause only error and fatal logs to be printed out. There is a function available for every log level from the level chosen all the way to the highest level. For example, if you were to choose `fatal` as your selected log level then an instance of this class would have a `fatal()` function available for you to use.
+
+## Available log levels:
+- debug
+- info
+- warn
+- error
+- fatal
+- always
+
+## Plugins
+- Additional functionality can be added to this logger in the form of plugins which are provided during instantiation.
+- For example, if you wanted to send certain log messages to slack, you can make use of the [slack plugin]() like so:
+
+```js
+const Logger = require('logger-nodejs');
+const SlackPlugin = require('logger-nodejs-slack-plugin');
+
+const slackPlugin = new SlackPlugin({
+    webhook: 'https://hooks.slack.com/services/your/webhook',
+    channel: 'errors',
+    username: 'API',
+    logLevel: 'error'
+});
+
+const log = new Logger({ plugins: [slackPlugin] });
+```
+
+- Now, every message of at least level `error` will be sent to the configured slack channel.
---
+++ src/index.js
@@ -0,0 +1,89 @@
+/* eslint no-console: 0 */
+
+'use strict';
+
+const DEFAULT_OPTS = { logMetadata: true, plugins: [] };
+const LOG_LEVELS = {
+    'debug': 0,
+    'info': 1,
+    'warn': 2,
+    'error': 3,
+    'fatal': 4,
+    'always': 5
+};
+
+/**
+ * Creates a logger that exposes various logging functions based on the selected log level.
+ * The selected log level acts as a filter with regards to how much you want printed out.
+ * If you choose 'info' as your log level then everything from info - fatal will be printed out.
+ * On the other hand if you want to only print out the most severe errors you could select 'error'
+ * as the log level which would cause only error and fatal logs to be printed out. There is a function
+ * available for every log level from the level chosen all the way to the highest level. For example,
+ * if you were to choose `fatal` as your selected log level then an instance of this class would have
+ * a `fatal()` function available for you to use.
+ */
+class Logger {
+    constructor(opts = {}) {
+        opts = { ...DEFAULT_OPTS, ...opts };
+        const logLevel = process.env['LOG_LEVEL'] || 'info';
+
+        // store the numerical value of the selected log level so we don't have to look it up every time we log
+        // something
+        this.SELECTED_LEVEL = LOG_LEVELS[logLevel.toLowerCase()];
+        this.name = opts.name;
+        this.plugins = opts.plugins;
+
+
+        // Create a function for each log level that is greater than or equal to the selected log level.
+        // For example, if the selected log level is `info` then we create the following functions:
+        //   info, warn, error fatal.
+        Object.keys(LOG_LEVELS).forEach(logLevel => { this[logLevel] = this.log(logLevel); });
+    }
+
+    log(level) {
+        return async function(text, opts = {}) {
+            const levelValue = LOG_LEVELS[level];
+
+            if (levelValue < this.SELECTED_LEVEL) return;
+
+            let timestamp = `<${new Date().toLocaleString()}>`;
+            text = `[${level.toUpperCase()}] ${timestamp} (${this.name}) -> ${text}`;
+
+            // serialize and log metadata if metadata logging is turned on.
+            if (this.logMetadata && opts.metadata) {
+                text += ` ${JSON.stringify(opts.metadata)}`;
+            }
+
+            console.log(text);
+
+            // run all attached plugins
+            for (let plugin of this.plugins) {
+                try {
+                    plugin.run({ text, level, levelValue, opts });
+                } catch (e) {
+                    console.log('plugin error:', e);
+
+                }
+            }
+        };
+    }
+
+    set name(name) {
+        if (name) {
+            this._name = `${name}-${process.pid}`;
+        } else {
+            this._name = process.pid;
+        }
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    static getLevelValue(level) {
+        return LOG_LEVELS[level];
+    }
+}
+
+// Export logger
+module.exports = Logger;
